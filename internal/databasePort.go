c2ef88b3f779b4a095f8029dba9141e2181b381f
michael anikammadu <dil.anikamadu@gmail.com>
2023-04-26 14:28:33 +0100

    database

--- internal/databasePort.go
+++ internal/databasePort.go
@@ -1,3 +1,10 @@
 package internal
 
-type IDatabase struct{}
+import "github.com/dilly3/blood-donor/internal/models"
+
+type IDatabase interface {
+	GetById(id string) (*models.Candidate, error)
+	GetAllCandidates() ([]*models.Candidate, error)
+	SaveCandidate(cand models.Candidate) (bool, error)
+	GetByFullname(name string) (*models.Candidate, error)
+}
--- internal/models/Candidate.go
+++ internal/models/Candidate.go
@@ -1,11 +1,23 @@
 package models
 
+import "github.com/lithammer/shortuuid"
+
 type Candidate struct {
-	Id         int64  `json:"id"`
-	FullName   string `json:"fullname"`
-	Mobile     string `json:"mobile"`
-	Email      string `json:"email"`
-	Age        int64  `json:"age"`
-	BloodGroup string `json:"blood_group"`
-	Address    string `json:"address"`
+	Id         string `json:"id" bson:"id"`
+	FullName   string `json:"fullname" bson:"fullname"`
+	Mobile     string `json:"mobile" bson:"mobile"`
+	Email      string `json:"email" bson:"email" validate:"email,required"`
+	Age        int64  `json:"age" bson:"age"`
+	BloodGroup string `json:"blood_group" bson:"blood_group"`
+	Address    string `json:"address" bson:"address"`
+}
+
+func GetshortId() string {
+	return shortuuid.New()
+}
+
+func NewCandidate() *Candidate {
+	return &Candidate{
+		Id: GetshortId(),
+	}
 }
